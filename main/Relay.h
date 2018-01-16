108405b0d518e259e6d76ddad830d104c277e233
M v/d Linde <marlonvdl@curve.group>
2018-01-16 17:35:50 +0200

    trivial code for class Sensors

--- main/Relay.h
+++ main/Relay.h
@@ -1,13 +1,13 @@
 /*
- * Relay.h - library for GreenButler specific relay control
- * Copyright Marlon van der Linde <marlon250f@gmail.com> 2018
- */
+   Relay.h - library for GreenButler specific relay control
+   Copyright Marlon van der Linde <marlon250f@gmail.com> 2018
+*/
 #ifndef Relay_h
 #define Relay_h
 
-#define PIN_RELAY1_SOLENOID  8                        
-#define PIN_RELAY2_FAN  9                        
-#define PIN_RELAY3_PUMP_A  10                        
+#define PIN_RELAY1_SOLENOID  8
+#define PIN_RELAY2_FAN  9
+#define PIN_RELAY3_PUMP_A  10
 #define PIN_RELAY4_PUMP_B  11
 
 class Relay
@@ -39,7 +39,7 @@ class Relay
     bool statePumpB = 0;
     bool stateFan = 0;
     bool stateSolenoid = 0;
-  
+
 };
 
 #endif
