27bc04d6e41f8097f073c4c2a9b8949dc4073fec
M v/d Linde <marlonvdl@curve.group>
2018-01-16 16:42:54 +0200

    Simplify relay control abstraction state tracking

--- main/Relay.h
+++ main/Relay.h
@@ -17,18 +17,18 @@ class Relay
     void begin();
 
     // Functions pertaining to starting and activating relay controlled devices
-    int startPumpA();
-    int startPumpB();
-    int startFan();
-    int openSolenoid();
+    void startPumpA();
+    void startPumpB();
+    void startFan();
+    void openSolenoid();
 
     // Functions for stopping and deactivating relay controlled devices
-    int stopPumpA();
-    int stopPumpB();
-    int stopFan();
-    int closeSolenoid();
+    void stopPumpA();
+    void stopPumpB();
+    void stopFan();
+    void closeSolenoid();
 
-    // Functions for checking the current status of relay controlled devices
+    // Functions for simply returning the current status of relay controlled devices
     bool statusPumpA();
     bool statusPumpB();
     bool statusFan();
